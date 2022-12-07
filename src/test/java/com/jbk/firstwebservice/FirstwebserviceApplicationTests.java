9a2a6b30b2323bb32c9fc5e8eabc94aef2d5cc04
Chandrakant <ichandrakant08@gmail.com>
2022-12-07 08:32:47 +0530

    project setup
    its all about setup

---
+++ src/test/java/com/jbk/firstwebservice/FirstwebserviceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.jbk.firstwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstwebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("test is ok");
+	}
+
+}
